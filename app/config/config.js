fb2c785c9482625e2df866502e9ff09daedfa62a
pabloc33 <pablo.contino33@gmail.com>
2021-08-07 19:35:31 -0300

    Tercer commit

--- app/config/config.js
+++ app/config/config.js
@@ -22,7 +22,6 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/evento";
 } else {
-  urlDB =
-    "mongodb+srv://root:root@cluster0.dztp9.mongodb.net/evento?retryWrites=true&w=majority";
+  urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
